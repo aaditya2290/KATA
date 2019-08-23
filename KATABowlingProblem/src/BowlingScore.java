c5f50ccfe9d907c65d4d75b23a296b68b0accd4e
aaditya2290 <aaditya_2290@yahoo.co.in>
2019-08-23 19:28:47 +0530

    handle scoring in case of multiple similar roll scores in game.

--- KATABowlingProblem/src/BowlingScore.java
+++ KATABowlingProblem/src/BowlingScore.java
@@ -2,12 +2,14 @@
 public class BowlingScore {
 	
 	public int score;
+	
     public int getScore()
-{
-    return 0;	
-}
+   {
+       return score;	
+   }
    public void roll(int pinsDown)
    {
+	   score+=pinsDown;
    }
     
 
--- KATABowlingProblem/src/BowlingScoreTest.java
+++ KATABowlingProblem/src/BowlingScoreTest.java
@@ -4,20 +4,39 @@ import org.junit.Test;
 
 
 public class BowlingScoreTest {
+	
+	BowlingScore score;
+	
+	public BowlingScoreTest()
+	{
+	 score=new BowlingScore();
+	}
 
 	@Test
 	public void testZeroBowlingScore() 
 	{
-		BowlingScore score=new BowlingScore();
+		
 		assertEquals(0,score.getScore());	
 	}
 	
 	@Test
 	public void testSingleRollBowlingScore() 
 	{
-		BowlingScore score=new BowlingScore();
 		score.roll(1);
 		assertEquals(1,score.getScore());	
+	}
+	@Test
+	public void test20RollBowlingScore() 
+	{
+		completeGame(20,1);
+		assertEquals(20,score.getScore());	
+	}
+	
+	//automate in case of same score for multiple consecutive rolls
+	public void completeGame(int rollCount, int pinsDown)
+	{
+		for (int i=0; i<rollCount;i++)
+		score.roll(pinsDown);	
 	}
 	
 	
