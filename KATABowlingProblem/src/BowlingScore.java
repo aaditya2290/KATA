483abee178cb667ea6f518f9e5c0ead4bae94bee
aaditya2290 <aaditya_2290@yahoo.co.in>
2019-08-23 19:13:15 +0530

    test case for single roll score -- failure

--- KATABowlingProblem/src/BowlingScore.java
+++ KATABowlingProblem/src/BowlingScore.java
@@ -1,11 +1,14 @@
 
 public class BowlingScore {
 	
+	public int score;
     public int getScore()
 {
-    	
     return 0;	
 }
+   public void roll(int pinsDown)
+   {
+   }
     
 
 }
--- KATABowlingProblem/src/BowlingScoreTest.java
+++ KATABowlingProblem/src/BowlingScoreTest.java
@@ -11,4 +11,14 @@ public class BowlingScoreTest {
 		BowlingScore score=new BowlingScore();
 		assertEquals(0,score.getScore());	
 	}
+	
+	@Test
+	public void testSingleRollBowlingScore() 
+	{
+		BowlingScore score=new BowlingScore();
+		score.roll(1);
+		assertEquals(1,score.getScore());	
+	}
+	
+	
 }
